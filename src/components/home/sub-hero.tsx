614556946ef505d22f08d3e1b543a6ffbbbb275c
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-20 00:36:39 +0100

    Add new style and format to the sub-hero

--- src/components/home/sub-hero.tsx
+++ src/components/home/sub-hero.tsx
@@ -5,7 +5,24 @@ const SubHero = () => {
   return (
     <section id='sub-hero'>
       <div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
-        <ul className='mt-8 grid grid-cols-1 gap-4 lg:grid-cols-3'>
+        <ul className='mt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+          <li>
+            <Link href='#' className='group relative block'>
+              <img
+                src='https://images.unsplash.com/photo-1618898909019-010e4e234c55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+                alt=''
+                className='aspect-square w-full object-cover transition duration-500 group-hover:opacity-70'
+              />
+              <div className='absolute inset-0 flex flex-col items-start justify-end p-6'>
+                <h3 className='text-xl font-medium text-white'>Casual Trainers</h3>
+                <span
+                  className='mt-1.5 inline-block bg-primary px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'
+                >
+                  Shop Now
+                </span>
+              </div>
+            </Link>
+          </li>
           <li>
             <Link href='#' className='group relative block'>
               <img
@@ -40,7 +57,41 @@ const SubHero = () => {
               </div>
             </Link>
           </li>
-          <li className='lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1'>
+          <li>
+            <Link href='#' className='group relative block'>
+              <img
+                src='https://images.unsplash.com/photo-1624623278313-a930126a11c3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+                alt=''
+                className='aspect-square w-full object-cover transition duration-500 group-hover:opacity-70'
+              />
+              <div className='absolute inset-0 flex flex-col items-start justify-end p-6'>
+                <h3 className='text-xl font-medium text-white'>Winter Jumpers</h3>
+                <span
+                  className='mt-1.5 inline-block bg-primary px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'
+                >
+                  Shop Now
+                </span>
+              </div>
+            </Link>
+          </li>
+          <li className='lg:col-span-2 lg:row-span-2 lg:row-start-1 lg:col-start-1 xl:col-span-2 xl:row-span-2 xl:col-start-3 xl:row-start-2'>
+            <Link href='#' className='group relative block'>
+              <img
+                src='https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80'
+                alt=''
+                className='aspect-square w-full object-cover transition duration-500 group-hover:opacity-70'
+              />
+              <div className='absolute inset-0 flex flex-col items-start justify-end p-6'>
+                <h3 className='text-xl font-medium text-white'>Skinny Jeans Blue</h3>
+                <span
+                  className='mt-1.5 inline-block bg-primary px-5 py-3 text-xs font-medium uppercase tracking-wide text-white'
+                >
+                  Shop Now
+                </span>
+              </div>
+            </Link>
+          </li>
+          <li className='xl:col-span-2 xl:row-span-2 xl:row-start-1'>
             <Link href='#' className='group relative block'>
               <img
                 src='https://images.unsplash.com/photo-1593795899768-947c4929449d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80'
