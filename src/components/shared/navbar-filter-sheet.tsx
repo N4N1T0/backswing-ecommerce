e93418ffefcb715a35e08d5add6c411a052594a0
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-20 12:13:49 +0100

    Add mobile navbar filter

---
+++ src/components/shared/navbar-filter-sheet.tsx
@@ -0,0 +1,20 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger
+} from '@/components/ui/sheet'
+import SideNavbarFilter from './side-navbar-filter'
+
+const NavbarFilterSheet = () => {
+  return (
+    <Sheet>
+      <SheetTrigger className='w-full backdrop:text-center border-[1px] border-secondary text-secondary bg-white hover:bg-secondary hover:text-white transition-colors duration-200 py-3'>Filtros</SheetTrigger>
+      <SheetContent side='left'>
+        <SideNavbarFilter />
+      </SheetContent>
+    </Sheet>
+
+  )
+}
+
+export default NavbarFilterSheet
