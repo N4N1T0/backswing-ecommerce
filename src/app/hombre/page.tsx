1ea01ae3463255cdf381edd5ebbf9c8f2fac5383
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-20 12:10:36 +0100

    Change routes to a hidden route

--- src/app/hombre/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HombrePage = () => {
-  return (
-    <div>HombrePage</div>
-  )
-}
-
-export default HombrePage
--- src/app/ninos/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NinosPage = () => {
-  return (
-    <div>NinosPage</div>
-  )
-}
-
-export default NinosPage
