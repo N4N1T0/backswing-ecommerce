b40a11e7dfd39c08ef7be669af14331c48dad475
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-21 22:20:23 +0100

    Change Promotion Dialog fix #2

--- src/components/shared/promotion-dialog.tsx
+++ src/components/shared/promotion-dialog.tsx
@@ -3,38 +3,34 @@ import {
   DialogContent,
   DialogTrigger
 } from '@/components/ui/dialog'
+import PromotionPic from '@/assets/artur-kornakov-ArI-foyWnfA-unsplash.webp'
+import Image from 'next/image'
 
 const PromotionDialog = ({ children }: { children: React.ReactNode }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className='overflow-hidden rounded-lg md:grid md:grid-cols-3 max-w-xl bg-gray-100 p-0'>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1611510338559-2f463335092c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80'
-          className='h-32 w-full object-cover md:h-full'
-        />
-
-        <div className='p-4 text-center sm:p-6 md:col-span-2 lg:p-8'>
-          <p className='text-sm font-semibold uppercase tracking-widest'>Run with the pack</p>
-
-          <h2 className='mt-6 font-black uppercase'>
-            <span className='text-4xl font-black sm:text-5xl lg:text-6xl'> Get 20% off </span>
-
-            <span className='mt-2 block text-sm'>On your next order over $50</span>
-          </h2>
-
-          <a
-            className='mt-8 inline-block w-full bg-black py-4 text-sm font-bold uppercase tracking-widest text-white'
-            href='#'
-          >
-            Get Discount
-          </a>
-
-          <p className='mt-8 text-xs font-medium uppercase text-gray-400'>
-            Offer valid until 24th March, 2021 *
-          </p>
+      <DialogContent className='flex bg-white'>
+        <div className='w-full px-4 mb-10 md:w-1/2 md:mb-0 relative text-center'>
+          <Image src={PromotionPic} alt='Un niño juagndo padel' title='Llevate un descuento para tu camiseta o la de tu hijo' fill />
         </div>
+        <form className='w-full px-4 md:w-1/2 '>
+          <h2 className='mb-4 text-3xl font-bold text-gray-950'>
+            20% Descuento
+          </h2>
+          <p className='mb-4 text-sm text-gray-700'>
+            Descuento esclusivo para los subscritos a nuestro newsletter, donde encontraras informacion actual sobre el padel y mucho mas!
+          </p>
+          <span className='block shadow-sm '>
+            <input type='email'
+              className='w-full px-4 py-4 mb-4 border border-gray-400 '
+              name='promotion-email' placeholder='Tu Email@...' required />
+            <button
+              className='inline-flex justify-center w-full px-4 py-4 text-white bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-600 hover:bg-gray-600 transition-colors duration-200' type='submit'>
+              Submit
+            </button>
+          </span>
+        </form>
       </DialogContent>
     </Dialog>
 
