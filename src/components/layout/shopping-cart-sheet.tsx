32fdd6c8b339ebca640cad5d6474a6e67181b18d
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-25 03:03:10 +0100

    Add Skeleton Search Page

--- src/components/layout/shopping-cart-sheet.tsx
+++ src/components/layout/shopping-cart-sheet.tsx
@@ -20,8 +20,14 @@ const ShoppingCartSheet = () => {
 
   return (
     <Sheet>
-      <SheetTrigger className='p-2 hover:bg-gray-300 rounded-lg transition-colors duration-300'>
+      <SheetTrigger className='p-2 hover:bg-gray-300 rounded-lg transition-colors duration-300 relative'>
         <span className='sr-only'>Shopping Cart Sheet</span>
+        {count.length !== 0 && (
+          <span className='absolute top-0 right-0 flex h-5 w-5'>
+            <span className='animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-950 opacity-25'></span>
+            <span className='inline-flex rounded-full h-full w-full bg-gray-950 justify-center items-center text-gray-100'>{count.length}</span>
+          </span>
+        )}
         <ShoppingCart />
       </SheetTrigger>
       <SheetContent className='bg-white justify-between items-center flex flex-col p-5'>
