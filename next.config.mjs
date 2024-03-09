7f8c13c7b230e24301c7cc3be91ff04847a159c5
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-03-10 00:43:35 +0100

    refactor: Add desiabled PWA when in development

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const withPWA = nextPwa({
   aggressiveFrontEndNavCaching: true,
   reloadOnOnline: true,
   swcMinify: true,
+  disable: process.env.NODE_ENV === 'development',
   workboxOptions: {
     disableDevLogs: true,
   }
