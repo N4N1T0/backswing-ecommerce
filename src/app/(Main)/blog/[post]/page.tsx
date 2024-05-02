48b1a7ef4f498031608d4f1ab21d9a83e190a340
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-02 15:57:10 +0200

    feat: connection to Wordpress Graphql Post

--- src/app/(Main)/blog/[post]/page.tsx
+++ src/app/(Main)/blog/[post]/page.tsx
@@ -1,118 +1,42 @@
 import Image from 'next/image'
-import BlogPostPic from '@/assets/parker-burchfield-tvG4WvjgsEY-unsplash.webp'
 import FeaturedBlogCard from '@/components/blog/featured-blog-card'
 import { type Metadata } from 'next'
+import { getPostById, getRelatedPost } from '@/lib/queries'
 
 export const metadata: Metadata = {
   title: 'Blog Post Mockup'
 }
 
-const BlogPost = ({ params }: { params: { post: string } }) => {
+const BlogPost = async ({ params }: { params: { post: string } }) => {
+  const post = await getPostById(params.post)
+  const relatedPosts = await getRelatedPost(params.post)
+  const { title, content, excerpt, featuredImage: { node: { mediaItemUrl, altText } } } = post
+
   return (
     <main className='mt-10 max-w-screen-2xl mx-auto p-5 md:p-10'>
 
       <div className='mb-4 md:mb-0 w-full mx-auto'>
         <div className='px-4 lg:px-0'>
           <h2 className='text-4xl font-semibold text-gray-900 leading-tight'>
-            Pellentesque a consectetur velit, ac molestie ipsum. Donec sodales, massa et auctor.
+            {title}
           </h2>
-          <a
-            href='#'
+          <div
             className='py-2 text-gray-700 inline-flex items-center justify-center mb-2'
+            dangerouslySetInnerHTML={{ __html: excerpt }}
           >
-            Cryptocurrency
-          </a>
+          </div>
         </div>
 
-        <Image src={BlogPostPic} alt='Una reunion de negocios de nuestro equipo' title='Una reunion de negocios de nuestro equipo' className='w-full object-cover lg:rounded' />
+        <Image src={mediaItemUrl} alt={altText} title={altText} className='w-full object-cover lg:rounded' width={1200} height={1000} />
       </div>
 
       <div className='flex flex-col lg:flex-row lg:space-x-12'>
-
-        <div className='px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4'>
-          <p className='pb-6'>Advantage old had otherwise sincerity dependent additions. It in adapted natural hastily is
-            justice. Six draw
-            you him full not mean evil. Prepare garrets it expense windows shewing do an. She projection advantages
-            resolution son indulgence. Part sure on no long life am at ever. In songs above he as drawn to. Gay was
-            outlived peculiar rendered led six.</p>
-
-          <p className='pb-6'>Difficulty on insensible reasonable in. From as went he they. Preference themselves me as
-            thoroughly
-            partiality considered on in estimating. Middletons acceptance discovered projecting so is so or. In or
-            attachment inquietude remarkably comparison at an. Is surrounded prosperous stimulated am me discretion
-            expression. But truth being state can she china widow. Occasional preference fat remarkably now projecting
-            uncommonly dissimilar. Sentiments projection particular companions interested do at my delightful. Listening
-            newspaper in advantage frankness to concluded unwilling.</p>
-
-          <p className='pb-6'>Adieus except say barton put feebly favour him. Entreaties unpleasant sufficient few pianoforte
-            discovered
-            uncommonly ask. Morning cousins amongst in mr weather do neither. Warmth object matter course active law
-            spring six. Pursuit showing tedious unknown winding see had man add. And park eyes too more him. Simple excuse
-            active had son wholly coming number add. Though all excuse ladies rather regard assure yet. If feelings so
-            prospect no as raptures quitting.</p>
-
-          <div className='border-l-4 border-gray-500 pl-4 mb-6 italic rounded'>
-            Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor
-            expression
-            unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence.
-            Steepest speaking up attended it as. Made neat an on be gave show snug tore.
-          </div>
-
-          <p className='pb-6'>Exquisite cordially mr happiness of neglected distrusts. Boisterous impossible unaffected he me
-            everything.
-            Is fine loud deal an rent open give. Find upon and sent spot song son eyes. Do endeavor he differed carriage
-            is learning my graceful. Feel plan know is he like on pure. See burst found sir met think hopes are marry
-            among. Delightful remarkably new assistance saw literature mrs favourable.</p>
-
-          <h2 className='text-2xl text-gray-800 font-semibold mb-4 mt-4'>Uneasy barton seeing remark happen his has</h2>
-
-          <p className='pb-6'>Guest it he tears aware as. Make my no cold of need. He been past in by my hard. Warmly thrown
-            oh he common
-            future. Otherwise concealed favourite frankness on be at dashwoods defective at. Sympathize interested
-            simplicity at do projecting increasing terminated. As edward settle limits at in.</p>
-
-          <p className='pb-6'>Dashwood contempt on mr unlocked resolved provided of of. Stanhill wondered it it welcomed oh.
-            Hundred no
-            prudent he however smiling at an offence. If earnestly extremity he he propriety something admitting convinced
-            ye. Pleasant in to although as if differed horrible. Mirth his quick its set front enjoy hoped had there. Who
-            connection imprudence middletons too but increasing celebrated principles joy. Herself too improve gay winding
-            ask expense are compact. New all paid few hard pure she.</p>
-
-          <p className='pb-6'>Breakfast agreeable incommode departure it an. By ignorant at on wondered relation. Enough at
-            tastes really
-            so cousin am of. Extensive therefore supported by extremity of contented. Is pursuit compact demesne invited
-            elderly be. View him she roof tell her case has sigh. Moreover is possible he admitted sociable concerns. By
-            in cold no less been sent hard hill.</p>
-
-          <p className='pb-6'>Detract yet delight written farther his general. If in so bred at dare rose lose good. Feel and
-            make two real
-            miss use easy. Celebrated delightful an especially increasing instrument am. Indulgence contrasted sufficient
-            to unpleasant in in insensible favourable. Latter remark hunted enough vulgar say man. Sitting hearted on it
-            without me.</p>
-
+        <div className='px-4 lg:px-0 mt-12 text-gray-700 text-lg leading-relaxed w-full lg:w-3/4 space-y-6' dangerouslySetInnerHTML={{ __html: content }}>
         </div>
 
         <div className='w-full lg:w-1/4 m-auto mt-12 max-w-screen-sm space-y-10'>
-          <div className='p-4 border-t border-b md:border md:rounded'>
-            <div className='flex py-2'>
-              <img src='https://randomuser.me/api/portraits/men/97.jpg'
-                className='h-10 w-10 rounded-full mr-2 object-cover' />
-              <div>
-                <p className='font-semibold text-gray-700 text-sm'> Mike Sullivan </p>
-                <p className='font-semibold text-gray-600 text-xs'> Editor </p>
-              </div>
-            </div>
-            <p className='text-gray-700 py-3'>
-              Mike writes about technology
-              Yourself required no at thoughts delicate landlord it be. Branched dashwood do is whatever it.
-            </p>
-            <button className='px-2 py-1 text-gray-100 bg-gray-700 flex w-full items-center justify-center rounded'>
-              Follow
-              <i className='bx bx-user-plus ml-2' ></i>
-            </button>
-          </div>
-          {Array.from({ length: 2 }).map((_, index) => (
-            <FeaturedBlogCard key={index} />
+          {relatedPosts.map((post) => (
+            <FeaturedBlogCard post={post} key={post.id} />
           ))}
         </div>
 
--- src/app/(Main)/blog/page.tsx
+++ src/app/(Main)/blog/page.tsx
@@ -1,5 +1,4 @@
 import BlogFeed from '@/components/blog/blog-feed'
-import FeaturedBlogs from '@/components/blog/featured-blog'
 import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -9,7 +8,6 @@ export const metadata: Metadata = {
 const BlogPage = () => {
   return (
     <main className='max-w-screen-3xl mx-auto p-10 space-y-10 text-center'>
-      <FeaturedBlogs />
       <BlogFeed />
     </main>
   )
