d9f9692777136cac74491428991edb877ba43f04
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-02 15:57:52 +0200

    feat: dynamic profile route for the users

--- src/app/(Main)/perfil/[id]/page.tsx
+++ src/app/(Main)/perfil/[id]/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'Perfil de Usuario'
 }
 
-const ProfilePage = () => {
+const ProfilePage = ({ params: { id } }: { params: { id: string } }) => {
   return (
     <main className='max-w-screen-3xl mx-auto p-5 md:p-10 space-y-5 md:space-y-10 text-center'>
       <Wishlist />
