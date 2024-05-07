26819aa0b5b7aac398e87d5cf9a0b47f5953f14c
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-07 19:37:15 +0200

    feat: Add tshirt models picture

---
+++ src/assets/models/index.ts
@@ -0,0 +1,6 @@
+import ModelA from './camisetas-mujer-model-a.webp'
+import ModelE from './camisetas-mujer-model-e.webp'
+import ModelD from './camisetas-mujer-model-d.webp'
+import ModelC from './camisetas-mujer-model-c.webp'
+
+export { ModelA, ModelE, ModelD, ModelC }
