3f3cb576f0d8435bcc70dead2499bbe17139cf84
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-17 18:15:15 +0200

    refactor: update orama client

--- src/lib/orama.ts
+++ src/lib/orama.ts
@@ -1,6 +1,6 @@
 import { OramaClient } from '@oramacloud/client'
 
 export const orama = new OramaClient({
-  endpoint: 'https://cloud.orama.run/v1/indexes/static-products-s2txoh',
-  api_key: 'NeIpmiB4I1oomcBo8V0ammFAJkFZ972U'
+  endpoint: 'https://cloud.orama.run/v1/indexes/backswing-products-yw9yfp',
+  api_key: 'FmfjLiIqpjuT0r2rr0mF1ydSziFjl8CC'
 })
