d3b619af194b41fdf81bc27b154a1403305da3f9
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-23 16:17:22 +0200

    refactor: No index as a key

--- src/app/(Main)/(products)/hombre/[category]/[product]/loading.tsx
+++ src/app/(Main)/(products)/hombre/[category]/[product]/loading.tsx
@@ -1,13 +1,16 @@
 export default function Loading() {
-  return (
-    <div className='h-screen w-full flex flex-col md:flex-row flex-wrap gap-6'>
-      {Array(2)
-        .fill(0)
-        .map((_, index) => {
-          return (
-            <div key={index} className='flex-1 animate-pulse bg-neutral-200 h-full rounded-lg' />
-          )
-        })}
-    </div>
-  )
+	return (
+		<div className='h-screen w-full flex flex-col md:flex-row flex-wrap gap-6'>
+			{Array(2)
+				.fill(0)
+				.map((_, index) => {
+					return (
+						<div
+							key={`hombre product card ${index + 1}`}
+							className='flex-1 animate-pulse bg-neutral-200 h-full rounded-lg'
+						/>
+					)
+				})}
+		</div>
+	)
 }
--- src/app/(Main)/(products)/loading.tsx
+++ src/app/(Main)/(products)/loading.tsx
@@ -1,15 +1,18 @@
-export default function Loading () {
-  return (
-    <article className='col-span-5 md:col-span-4 space-y-5 h-full'>
-      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 md:gap-y-10 h-full'>
-        {Array(12)
-          .fill(0)
-          .map((_, index) => {
-            return (
-              <div key={index} className='animate-pulse bg-neutral-200 h-full w-auto rounded-lg aspect-square' />
-            )
-          })}
-      </div>
-    </article>
-  )
+export default function Loading() {
+	return (
+		<article className='col-span-5 md:col-span-4 space-y-5 h-full'>
+			<div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 md:gap-y-10 h-full'>
+				{Array(12)
+					.fill(0)
+					.map((_, index) => {
+						return (
+							<div
+								key={`Categories product card ${index + 1}`}
+								className='animate-pulse bg-neutral-200 h-full w-auto rounded-lg aspect-square'
+							/>
+						)
+					})}
+			</div>
+		</article>
+	)
 }
