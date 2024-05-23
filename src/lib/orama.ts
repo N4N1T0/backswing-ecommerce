6faecd7fd3035ede12488a8aad4666ed7c496c57
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-23 16:16:48 +0200

    refactor: Orama fix

--- src/lib/orama.ts
+++ src/lib/orama.ts
@@ -1,6 +1,6 @@
 import { OramaClient } from '@oramacloud/client'
 
 export const orama = new OramaClient({
-  endpoint: 'https://cloud.orama.run/v1/indexes/backswing-products-yw9yfp',
-  api_key: 'FmfjLiIqpjuT0r2rr0mF1ydSziFjl8CC'
+	endpoint: 'https://cloud.orama.run/v1/indexes/backswing-products-yw9yfp',
+	api_key: 'FmfjLiIqpjuT0r2rr0mF1ydSziFjl8CC',
 })
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,139 +1,120 @@
-// TODO Eliminar despues de subir todos los productos
-export interface StaticProductsTypes {
-    id: string
-    name: string
-    price: number
-    description: string
-    material: string
-    image: string
-    gender: 'mujer' | 'hombre' | 'ninos'
-    new: boolean
-    offer: {
-      price: number
-      onOffer: boolean
-    }
-    category: string
-    quantity: number
-  }
-
-export interface WPPosts {
-  posts: {
-    nodes: WPPost[]
-  }
-}
+import type { StaticImageData } from 'next/image'
 
 export interface WPPost {
-  post: {
-     title: string
-      content: string
-      excerpt: string
-      id: string
-      date: string
-      slug: string
-      featuredImage: {
-        node: {
-          mediaItemUrl: string
-          altText: string
-        }
-      }
-  }
+	title: string
+	content: string
+	excerpt: string
+	id: string
+	date: string
+	slug: string
+	featuredImage: {
+		node: {
+			mediaItemUrl: string
+			altText: string
+		}
+	}
 }
 
 export interface WPProduct {
-    id: string
-    image: {
-      sourceUrl: string
-    }
-    name: string
-    price: string
-    date: string
-    onSale: boolean
-    content: string
-    variations: Variations
-    productCategories: Categories
-    attributes: Attributes
-    upsell: Related
+	id: string
+	image: {
+		sourceUrl: string
+	}
+	name: string
+	price: string
+	date: string
+	onSale: boolean
+	content: string
+	variations: Variations
+	productCategories: Categories
+	attributes: Attributes
+	upsell: Related
 }
 
-export interface StaticWPProducts extends Omit<WPProduct, 'upsell' | 'content' | 'variations' | 'attributes'> {}
+export interface StaticWPProducts
+	extends Omit<WPProduct, 'upsell' | 'content' | 'variations' | 'attributes'> {}
 
 export interface CartItem {
-    id: string
-    talla: string
-    model: Variations['nodes'][0]
-    parsedPrice: string
-    parsedName: string
-    quantity: number
-    description: string
+	id: string
+	talla: string
+	model: Variations['nodes'][0]
+	parsedPrice: string
+	parsedName: string
+	quantity: number
+	description: string
 }
 
 export interface Variations {
-  nodes: Array<{
-    image: {
-      sourceUrl: string
-    }
-    name: string
-  }>
+	nodes: Array<{
+		image: {
+			sourceUrl: string
+		}
+		name: string
+	}>
 }
 
 export interface Categories {
-  nodes: Array<{
-    name: string
-  }>
+	nodes: Array<{
+		name: string
+	}>
 }
 
 export interface Attributes {
-  nodes: Array<{
-    options: string[]
-  }>
+	nodes: Array<{
+		options: string[]
+	}>
 }
 
 export interface Related {
-  nodes: Array<{
-    name: string
-    id: string
-    variations: Variations
-  }>
-}
-
-export interface Collection {
-  nodes: Array<{
-  id: string
-  name: string
-  image: {
-    sourceUrl: string
-  }
-  price: string
-  productCategories: Categories
-}>
+	nodes: Array<{
+		name: string
+		id: string
+		variations: Variations
+	}>
 }
 
 export interface User {
-  nodes: Costumer[]
+	nodes: Costumer[]
 }
 
 export interface Costumer {
-    email: string
-    id: string
-    username: string
+	email: string
+	id: string
+	username: string
 }
 
 export interface ParsedConstent {
-  description: string
-    material: string
-    parsedName: string
-    variations: Variations
-    isNew: boolean
-    parsedPrice: string
-    category: string | undefined
-    gender: string | undefined
-    colors: string[]
-    image: {
-      sourceUrl: string
-    }
-    id: string
-    onSale: boolean
-    related: Related | null
+	description: string
+	material: string
+	parsedName: string
+	variations: Variations
+	isNew: boolean
+	parsedPrice: string
+	category: string | undefined
+	gender: string | undefined
+	colors: string[]
+	image: {
+		sourceUrl: string
+	}
+	id: string
+	onSale: boolean
+	related: Related | null
 }
 
-export interface ParsedStaticProduct extends Omit<ParsedConstent, 'description' | 'material' | 'variations' | 'colors' | 'related' > {}
+export interface ParsedStaticProduct
+	extends Omit<
+		ParsedConstent,
+		'description' | 'material' | 'variations' | 'colors' | 'related'
+	> {}
+
+export interface PersonalizationProducts {
+	value: string
+	image: StaticImageData
+	colors: string[] | null
+}
+
+export interface OramaHit {
+	id: string
+	score: number
+	document: WPProduct
+}
