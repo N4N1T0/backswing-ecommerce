573bb0267832b42bc69b09e20afe1c677be0f810
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-05-23 16:15:09 +0200

    feat: Add Static Products for the Home Page

---
+++ src/assets/personalization/index.ts
@@ -0,0 +1,41 @@
+import CamisetaMujer from './camiseta-mujer.webp'
+import CamisetaHombre from './camisetas-hombre.webp'
+import CamisetaNino from './camiseta-niño.webp'
+import SudaderasUnisex from './sudaderas-unisex.webp'
+import SudaderasNino from './sudaderas-nino.webp'
+import FirstDay from './DISEÑO1.png'
+import LovePadel from './DISEÑO2.png'
+import StrongAndFurius from './DISEÑO3.png'
+import Mirones from './DISEÑO4.png'
+import Teenagers from './DISEÑO5.png'
+import NoAge from './DISEÑO6.png'
+import Timming from './DISEÑO7.png'
+import JuegasConmigo from './DISEÑO8.png'
+import Locomotive from './DISEÑO9.png'
+import Evolution from './DISEÑO10.png'
+import Extreme from './DISEÑO11.png'
+import FashionGirl from './DISEÑO12.png'
+import HardMatch from './DISEÑO13.png'
+import Family from './DISEÑO14.png'
+
+export {
+	CamisetaMujer,
+	CamisetaHombre,
+	CamisetaNino,
+	SudaderasUnisex,
+	SudaderasNino,
+	FirstDay,
+	LovePadel,
+	StrongAndFurius,
+	Mirones,
+	Teenagers,
+	NoAge,
+	Timming,
+	JuegasConmigo,
+	Locomotive,
+	Evolution,
+	Extreme,
+	FashionGirl,
+	HardMatch,
+	Family,
+}
