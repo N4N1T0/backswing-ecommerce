9f128ed0ee2c38a5aa56e17c06b89bed30b179c6
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-01 15:25:09 +0200

    refactor: Add some comments to the Home Components

--- src/components/home/featured.tsx
+++ src/components/home/featured.tsx
@@ -1,41 +1,33 @@
-import { parseStaticProductContent } from '@/lib/utils'
-import { newProducts as products } from '@/contants/static-products'
+// Next.js Imports
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Featured = () => {
-	/**
-	 * This component renders the "Featured" section on the home page.
-	 * It's a grid of products, with a heading, a paragraph, and
-	 * links to view all products in the featured collection.
-	 *
-	 * The grid is divided into three columns on medium screens and
-	 * four columns on large screens. The first column contains the
-	 * heading and paragraph, while the other columns contain the
-	 * product grid.
-	 */
+// Utils Imports
+import { parseStaticProductContent } from '@/lib/utils'
 
+// Data Imports
+import { newProducts as products } from '@/contants/static-products'
+
+/**
+ * Renders the featured section of the homepage.
+ *
+ * @return {JSX.Element} The featured section component.
+ */
+const Featured = (): JSX.Element => {
 	return (
-		<section>
-			{/* The container for the featured section. It has some padding
-      and is centered on medium screens. */}
+		<section id='featured'>
 			<div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
-				{/* The grid container. It's divided into columns on medium screens and
-        large screens. The first column contains the heading and paragraph, while
-        the other columns contain the product grid. */}
+				{/* Grid container for the featured section */}
 				<div className='grid grid-cols-1 gap-4 lg:grid-cols-3 xl:grid-cols-4 lg:items-stretch'>
-					{/* The first column contains the heading and paragraph. */}
+					{/* Featured text and link section */}
 					<div className='grid place-content-center bg-gray-300 p-6 sm:p-8'>
-						{/* The container for the heading and paragraph. */}
 						<div className='mx-auto max-w-md text-center lg:text-left'>
-							{/* The heading. It's a h2 element with a font size class that
-              makes it larger on medium screens. */}
+							{/* Heading */}
 							<header>
 								<h2 className='text-xl font-bold text-gray-900 sm:text-3xl uppercase text-wrap'>
 									Sudaderas Unisex
 								</h2>
-								{/* The paragraph. It explains what the featured collection is
-                and why it's cool. */}
+								{/* Description */}
 								<p className='mt-4 text-gray-700'>
 									Explora nuestra nueva colección de sudaderas, gorros y más.
 									Con calidad premium y diseños vanguardistas, cada artículo
@@ -43,11 +35,7 @@ const Featured = () => {
 									moda.
 								</p>
 							</header>
-							{/* The unordered list of links to view all products in the
-              featured collection. */}
-							<ul />
-							{/* The link to view all products in the featured collection.
-              It's styled with a hover effect that changes the background color. */}
+							{/* Link to the products page */}
 							<Link
 								href='/nuevo/sudaderas'
 								className='mt-8 inline-block bg-gray-950 px-12 py-3 text-sm font-medium text-white transition-colors duration-200 hover:bg-gray-700'
@@ -56,26 +44,17 @@ const Featured = () => {
 							</Link>
 						</div>
 					</div>
-					{/* The second column contains the product grid. */}
+					{/* Products section */}
 					<div className='lg:col-span-2 xl:col-span-3 lg:py-8'>
-						{/* The container for the product grid. */}
 						<ul className='grid grid-cols-2 xl:grid-cols-4 gap-4'>
-							{/* The products are rendered in a loop using the map function. */
-							/* Each product is rendered as a li element with a link to the
-              product page. The link contains a group hover effect that
-              changes the opacity of the image. The image is also styled
-              with a aspect ratio and object centering. The h3 and p
-              elements contain the product name and price, respectively. */}
+							{/* Mapping through the products */}
 							{products.map((product) => {
-								const { category, gender, id, image, parsedPrice, parsedName } =
+								const { id, image, parsedPrice, parsedName } =
 									parseStaticProductContent(product)
 
 								return (
 									<li key={product.id} className='relative'>
-										<Link
-											href={`/${gender?.toLowerCase()}/${category?.toLowerCase()}/${id}`}
-											className='group block'
-										>
+										<Link href={`/sudaderas/${id}`} className='group block'>
 											<Image
 												src={image.sourceUrl}
 												alt={parsedName}
--- src/components/home/sub-hero.tsx
+++ src/components/home/sub-hero.tsx
@@ -1,60 +1,59 @@
+// Next.js Imports
 import Link from 'next/link'
 import Image from 'next/image'
+
+// Utils Imports
 import { parseStaticProductContent } from '@/lib/utils'
+
+// Data Imports
 import { subHeroStaticProducts as products } from '@/contants/static-products'
 
-// This is a functional component that fetches the latest 5 products from the API and renders them in a grid.
-// The component is a section with the id 'sub-hero'.
-// It uses Tailwind CSS classes to style the layout.
-const SubHero = () => {
-	// Render the product grid.
-	return (
-		<section id='sub-hero'>
-			{/* Center the grid horizontally and add padding. */}
-			<div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
-				{/* Create a grid with 1 column on small devices, 2 columns on medium devices, 3 columns on large devices, and 4 columns on extra large devices. */}
-				<ul className='mt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-					{/* Loop through each product. */}
-					{products.map((product, index) => {
-						// Parse the product content to extract the category, gender, ID, image, description, and parsed name.
-						const { category, gender, id, image, parsedName } =
-							parseStaticProductContent(product)
+/**
+ * Renders the SubHero section of the webpage with dynamic product content.
+ *
+ * @return {JSX.Element} The JSX element representing the SubHero section.
+ */
+const SubHero = (): JSX.Element => (
+	<section id='sub-hero'>
+		<div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
+			<ul className='mt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+				{products.map((product, index) => {
+					// Parse the static product data
+					const { category, gender, id, image, parsedName } =
+						parseStaticProductContent(product)
 
-						// Render a grid item with a link to the product page.
-						return (
-							<li
-								key={id}
-								className={`${
-									index === 4
-										? 'lg:col-span-2 lg:row-span-2 lg:row-start-1 lg:col-start-1'
-										: ''
-								} group relative block overflow-hidden`}
+					return (
+						<li
+							key={id}
+							// Apply different styles to the 5th item to make it larger
+							className={`${
+								index === 4
+									? 'lg:col-span-2 lg:row-span-2 lg:row-start-1 lg:col-start-1'
+									: ''
+							} group relative block overflow-hidden`}
+						>
+							<Link
+								href={`/${gender?.toLowerCase()}/${category?.toLowerCase()}/${id}`}
 							>
-								{/* Link to the product page. */}
-								<Link
-									href={`/${gender?.toLowerCase()}/${category?.toLowerCase()}/${id}`}
-								>
-									{/* Render the product image. */}
-									<Image
-										src={image.sourceUrl}
-										alt={parsedName}
-										className='aspect-square object-center transition-transform duration-300 ease-out group-hover:scale-110 w-auto h-auto'
-										width={index === 4 ? 1000 : 400}
-										height={index === 4 ? 1000 : 400}
-										priority
-									/>
-									{/* Render the product name as a heading. */}
-									<h3 className='text-xl text-gray-100 uppercase font-semibold bg-gray-900 px-3 py-2 absolute bottom-3 left-3'>
-										{parsedName}
-									</h3>
-								</Link>
-							</li>
-						)
-					})}
-				</ul>
-			</div>
-		</section>
-	)
-}
+								<Image
+									src={image.sourceUrl}
+									alt={parsedName}
+									className='aspect-square object-center transition-transform duration-300 ease-out group-hover:scale-110 w-auto h-auto'
+									width={index === 4 ? 1000 : 400}
+									height={index === 4 ? 1000 : 400}
+									priority
+								/>
+								{/* Display the product name on top of the image */}
+								<h3 className='text-xl text-gray-100 uppercase font-semibold bg-gray-900 px-3 py-2 absolute bottom-3 left-3'>
+									{parsedName}
+								</h3>
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	</section>
+)
 
 export default SubHero
