3e9a73dcd0339b1171b779ee7fe87c1cc95624c9
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-01 15:04:31 +0200

    refator: Add some comments to the checkout components

--- src/components/checkout/checkout-quantity.tsx
+++ src/components/checkout/checkout-quantity.tsx
@@ -1,29 +1,49 @@
 'use client'
 
-import useShoppingCart from '@/stores/shopping-cart-store'
-import type { CartItem } from '@/types'
+// React Imports
 import { useState } from 'react'
 
-const CheckoutQuantity = ({ product }: { product: CartItem }) => {
+// Stores Imports
+import useShoppingCart from '@/stores/shopping-cart-store'
+
+// Types Imports
+import type { CartItem } from '@/types'
+
+/**
+ * Renders a quantity input component for a product in the checkout page.
+ *
+ * @param {Object} props - The component props.
+ * @param {CartItem} props.product - The product to update the quantity for.
+ * @return {JSX.Element} The rendered quantity input component.
+ */
+const CheckoutQuantity = ({ product }: { product: CartItem }): JSX.Element => {
+	// State to hold the current quantity of the product
 	const [, setCount] = useShoppingCart()
 	const [quantity, setQuantity] = useState(product.quantity)
 
+	// Function to increase the quantity of the product
 	const plusQuantity = () => {
+		// Update the count state by mapping over the items and updating the quantity if the item id matches the current product id
 		setCount((prev) =>
 			prev.map((item) =>
 				item.id === product.id ? { ...item, quantity: quantity + 1 } : item,
 			),
 		)
+		// Update the quantity state by increasing it by 1
 		setQuantity((prev) => prev + 1)
 	}
 
+	// Function to decrease the quantity of the product
 	const minusQuantity = () => {
+		// If the quantity is already 1, do nothing
 		if (quantity === 1) return
+		// Update the count state by mapping over the items and updating the quantity if the item id matches the current product id
 		setCount((prev) =>
 			prev.map((item) =>
 				item.id === product.id ? { ...item, quantity: quantity - 1 } : item,
 			),
 		)
+		// Update the quantity state by decreasing it by 1
 		setQuantity((prev) => prev - 1)
 	}
 
