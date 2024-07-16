5ca597f0de75df69d373ae592e07c2d36ec1666f
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-07-16 12:28:13 +0200

    fix: Add new orama index with new Items

--- src/lib/orama.ts
+++ src/lib/orama.ts
@@ -1,8 +1,12 @@
 // Orama Imports
 import { OramaClient } from '@oramacloud/client'
 
+// Orama Variables
+const endpoint = process.env.ORAMA_API_ENDPOINT || ''
+const apiKey = process.env.ORAMA_API_KEY || ''
+
 // Orama Client
 export const orama = new OramaClient({
-	endpoint: 'https://cloud.orama.run/v1/indexes/backswing-products-yw9yfp',
-	api_key: 'FmfjLiIqpjuT0r2rr0mF1ydSziFjl8CC',
+	endpoint: 'https://cloud.orama.run/v1/indexes/backswing-products-f9m76l',
+	api_key: 'EdX84dEdShgqz5oRkG5vkc0ph2uEpsBL',
 })
