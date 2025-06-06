56d00c180075bae9c2026cab5c0f41fa676f6398
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-06 16:05:27 +0200

    chore: Update to the placeholder api image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,26 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: 's7g3.scene7.com',
-			},
-			{
-				protocol: 'https',
-				hostname: 'i.postimg.cc',
-			},
-			{
-				protocol: 'https',
-				hostname: 'backswingpadel.com',
-			},
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's7g3.scene7.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc'
+      },
+      {
+        protocol: 'https',
+        hostname: 'backswingpadel.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co'
+      }
+    ]
+  }
 }
 
 export default nextConfig
