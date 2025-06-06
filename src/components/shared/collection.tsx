6cacfb32241d0b376dcde046e2747989645f30af
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-06 16:03:21 +0200

    chore: Update shared components

--- src/components/shared/collection.tsx
+++ src/components/shared/collection.tsx
@@ -1,118 +1,85 @@
-// Next.js Imports
+import {
+  collectionMenStaticProducts,
+  collectionWomenStaticProducts
+} from '@/contants/static-products'
+import { parseStaticProductContent, useCapitalize } from '@/lib/utils'
+import Image from 'next/image'
 import Link from 'next/link'
 
-// Utils Imports
-import { useCapitalize, parseStaticProductContent } from '@/lib/utils'
-
-// Next.js Image
-import Image from 'next/image'
-
-// Assets Imports
-import {
-	collectionMenStaticProducts,
-	collectionWomenStaticProducts,
-} from '@/contants/static-products'
-
-/**
- * Renders a collection section based on the provided direction and collection type.
- *
- * @param {string} direction - The direction of the collection section ('right' or 'left').
- * @param {string} props.collection - The type of collection ('hombre' for men, 'mujer' for women, or 'niño' for children).
- * @return {JSX.Element} The collection section JSX element.
- */
 const Collection = ({
-	direction,
-	collection,
+  direction,
+  collection
 }: {
-	direction: 'right' | 'left'
-	collection: 'hombre' | 'mujer' | 'niño'
+  direction: 'right' | 'left'
+  collection: 'hombre' | 'mujer' | 'niño'
 }): JSX.Element => {
-	// Determine the appropriate set of static products based on the collection type
-	const products =
-		collection === 'hombre'
-			? collectionMenStaticProducts
-			: collectionWomenStaticProducts
+  const products =
+    collection === 'hombre'
+      ? collectionMenStaticProducts
+      : collectionWomenStaticProducts
 
-	return (
-		<section id={`${collection}-collection`}>
-			<div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
-				{/* Header section with title and description */}
-				<header
-					className={`w-full flex flex-col ${
-						direction === 'right' ? 'items-end' : 'items-start'
-					}`}
-				>
-					<h2 className='text-xl font-bold text-gray-900 sm:text-3xl'>
-						{useCapitalize(collection)}
-					</h2>
+  return (
+    <section id={`${collection}-collection`}>
+      <div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8'>
+        <header
+          className={`w-full flex flex-col ${
+            direction === 'right' ? 'items-end' : 'items-start'
+          }`}
+        >
+          <h2 className='text-xl font-bold text-gray-900 sm:text-3xl'>
+            {useCapitalize(collection)}
+          </h2>
+          <p
+            className={`${
+              direction === 'right' ? 'text-right' : 'text-left'
+            } mt-4 max-w-md text-gray-500`}
+          >
+            Descubre nuestra nueva colección de camisetas para {collection}
+          </p>
+        </header>
 
-					{/* Display description based on the collection type */}
-					{collection === 'hombre' ? (
-						<p
-							className={`${
-								direction === 'right' ? 'text-right' : 'text-left'
-							} mt-4 max-w-md text-gray-500`}
-						>
-							Descubre nuestra nueva coleccion de camisetas para Hombres
-						</p>
-					) : (
-						<p
-							className={`${
-								direction === 'right' ? 'text-right' : 'text-left'
-							} mt-4 max-w-md text-gray-500`}
-						>
-							Descubre nuestra nueva coleccion de camisetas para Mujeres
-						</p>
-					)}
-				</header>
+        <ul className='mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
+          {products.map((product) => {
+            const { category, gender, id, image, parsedName, parsedPrice } =
+              parseStaticProductContent(product)
 
-				{/* Product grid */}
-				<ul className='mt-8 grid gap-4 sm:grid-cols-2 lg:grid-cols-4'>
-					{/* Map over the products and render a product card for each */}
-					{products.map((product) => {
-						const { category, gender, id, image, parsedName, parsedPrice } =
-							parseStaticProductContent(product)
+            return (
+              <li key={product.id} className='relative'>
+                <Link
+                  href={`/${gender?.toLocaleLowerCase()}/${category?.toLocaleLowerCase()}/${id}`}
+                  className='group block overflow-hidden bg-linear-to-r from-[#A8A8A8] from-50% to-50% to-white'
+                >
+                  <Image
+                    src={image.sourceUrl}
+                    alt={parsedName}
+                    title={parsedName}
+                    width={350}
+                    height={450}
+                    loading='lazy'
+                    className='w-full h-auto object-contain transition-transform ease-out duration-300 group-hover:scale-105 aspect-square'
+                  />
 
-						return (
-							<li key={product.id} className='relative'>
-								{/* Link to the product page */}
-								<Link
-									href={`/${gender?.toLocaleLowerCase()}/${category?.toLocaleLowerCase()}/${id}`}
-									className='group block overflow-hidden bg-linear-to-r from-[#A8A8A8] from-50% to-50% to-white'
-								>
-									{/* Product image */}
-									<Image
-										src={image.sourceUrl}
-										alt={parsedName}
-										title={parsedName}
-										width={350}
-										height={450}
-										loading='lazy'
-										className='w-full h-auto object-contain transition-transform ease-out duration-300 group-hover:scale-105 aspect-square'
-									/>
+                  <div className='relative bg-white pt-3'>
+                    <h3 className='text-gray-700 group-hover:underline group-hover:underline-offset-4 uppercase font-medium'>
+                      {parsedName}
+                    </h3>
 
-									{/* Product details */}
-									<div className='relative bg-white pt-3'>
-										<h3 className='text-gray-700 group-hover:underline group-hover:underline-offset-4 uppercase font-medium'>
-											{parsedName}
-										</h3>
+                    <p className='mt-2'>
+                      <span className='sr-only'>Regular Price</span>
 
-										<p className='mt-2'>
-											<span className='sr-only'>Regular Price</span>
-
-											<span className='tracking-wider text-gray-900'>
-												{parsedPrice}
-											</span>
-										</p>
-									</div>
-								</Link>
-							</li>
-						)
-					})}
-				</ul>
-			</div>
-		</section>
-	)
+                      <span className='tracking-wider text-gray-900'>
+                        {parsedPrice}
+                      </span>
+                    </p>
+                  </div>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Collection
--- src/components/shared/pre-footer.tsx
+++ src/components/shared/pre-footer.tsx
@@ -1,68 +1,41 @@
-// Assets Imports
 import { medioAmbiente } from '@/contants'
-
-// Next.js Imports
 import Image from 'next/image'
-import Link from 'next/link'
 
-/**
- * Renders the PreFooter section with a focus on environmental commitment.
- *
- * @return {JSX.Element} The JSX element representing the PreFooter section.
- */
 const PreFooter = (): JSX.Element => {
-	return (
-		<section id='pre-footer'>
-			{/* Container for the PreFooter section */}
-			<div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 lg:px-8'>
-				{/* Grid for the PreFooter items */}
-				<div className='grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-4'>
-					{/* Content section */}
-					<div className='bg-gray-950 p-6 2xl:p-14 mx-auto max-w-xl space-y-5 flex justify-center items-center flex-col text-center'>
-						{/* Title */}
-						<h2 className='text-2xl font-bold text-white 2xl:text-3xl'>
-							Compromiso con el medio Ambiente
-						</h2>
+  return (
+    <section id='pre-footer'>
+      <div className='mx-auto max-w-screen-3xl px-4 py-8 sm:px-6 lg:px-8'>
+        <div className='grid gap-4 grid-cols-1 md:grid-cols-4'>
+          <div className='bg-gray-950 p-5 space-y-5 flex justify-center items-center flex-col text-center'>
+            <h2 className='text-xl font-bold text-white 2xl:text-3xl'>
+              Compromiso con el medio Ambiente
+            </h2>
 
-						{/* List of commitments */}
-						<ul className='text-gray-100 space-y-3'>
-							{medioAmbiente.map((item) => (
-								<li key={item.label} className='space-y-2'>
-									<h3 className='font-bold text-base 2xl:text-lg'>
-										{item.label}
-									</h3>
-									<p className='text-xs 2xl:text-sm text-gray-300'>
-										{item.description}
-									</p>
-								</li>
-							))}
-						</ul>
-
-						{/* Link to products page */}
-						<div className='mt-4 md:mt-8'>
-							<Link
-								href='/hombre/camisetas'
-								className='mt-8 inline-block bg-gray-100 px-12 py-3 text-sm font-medium text-gray-950 transition-colors duration-200 hover:bg-gray-400'
-							>
-								Comprar ahora
-							</Link>
-						</div>
-					</div>
-					{/* Images section */}
-					{medioAmbiente.map((item) => (
-						<Image
-							key={`image-${item.label}`}
-							src={item.image}
-							alt={item.label}
-							title={item.label}
-							loading='lazy'
-							className='w-full h-auto'
-						/>
-					))}
-				</div>
-			</div>
-		</section>
-	)
+            <ul className='text-gray-100 space-y-3'>
+              {medioAmbiente.map((item) => (
+                <li key={item.label} className='space-y-2'>
+                  <h3 className='font-bold text-base 2xl:text-lg'>
+                    {item.label}
+                  </h3>
+                  <p className='text-xs text-gray-300'>{item.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          {medioAmbiente.map((item) => (
+            <Image
+              key={`image-${item.label}`}
+              src={item.image}
+              alt={item.label}
+              title={item.label}
+              loading='lazy'
+              className='w-full h-auto'
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default PreFooter
