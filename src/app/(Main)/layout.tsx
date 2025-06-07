b55744cf8c0d400cf48c3a958a70494e45606e3a
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-07 19:13:11 +0200

    feat: Prepare the Main Layout

--- src/app/(Main)/layout.tsx
+++ src/app/(Main)/layout.tsx
@@ -1,24 +1,16 @@
-// Components Imports
-import Navbar from '@/components/layout/navbar'
 import Footer from '@/components/layout/footer'
+import Navbar from '@/components/layout/navbar'
 
-/**
- * Renders the layout component with a Navbar, the provided children, and a Footer.
- *
- * @param {Object} props - The properties for the Layout component.
- * @param {React.ReactNode} props.children - The child components to be rendered.
- * @return {JSX.Element} The rendered Layout component.
- */
 export default function Layout({
-	children,
+  children
 }: Readonly<{
-	children: React.ReactNode
-}>): JSX.Element {
-	return (
-		<>
-			<Navbar />
-			{children}
-			<Footer />
-		</>
-	)
+  children: React.ReactNode
+}>) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
 }
