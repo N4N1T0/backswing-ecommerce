a74d5b989c360fc4dee1cf989fa1302550d57779
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-06-23 22:03:53 +0200

    refactor(side-navbar-filter): replace Link with a tags for simplicity
    The change removes Next.js Link components in favor of standard a tags since the additional features of Link weren't being utilized. This simplifies the component while maintaining the same functionality.

--- src/components/shared/side-navbar-filter.tsx
+++ src/components/shared/side-navbar-filter.tsx
@@ -8,7 +8,6 @@ import {
 } from '@/components/ui/accordion'
 import { sidebarAccordion } from '@/contants'
 import { accordionPathname } from '@/lib/utils'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
 const SideNavbarFilter = () => {
@@ -19,17 +18,13 @@ const SideNavbarFilter = () => {
   )
 
   return (
-    // Render an accordion component
     <Accordion type='multiple' className='pr-5' defaultValue={accordionPath}>
-      {/* Loop over the sidebarAccordion array */}
       {sidebarAccordion.map((accordion) => (
-        // Render an accordion item
         <AccordionItem
           value={accordion.label}
           key={accordion.label}
           className='relative'
         >
-          {/* Render the accordion trigger */}
           <AccordionTrigger
             disabled={accordion.commingSoon}
             className={`${
@@ -38,31 +33,27 @@ const SideNavbarFilter = () => {
                 : ''
             }`}
           >
-            {/* Display 'Niños' instead of 'Ninos' */}
             {accordion.label === 'Ninos' ? 'Niños' : accordion.label}
           </AccordionTrigger>
-          {/* Display a 'pronto' badge if the accordion is comming soon */}
           {accordion.commingSoon && (
             <span className='absolute top-2 -right-1 text-xs text-white bg-red-500 rounded-full px-1.5 py-0.5 pointer-events-none'>
               pronto
             </span>
           )}
-          {/* Render the accordion content */}
           <AccordionContent className='space-y-2'>
-            {/* Loop over the products in the accordion and render a link for each */}
             {accordion.products.map((product) => (
               <li
                 key={product.label}
                 className='list-none ml-5 hover:text-gray-500 duration-200 transition-colors'
               >
-                <Link href={product.route}>{product.label}</Link>
+                <a href={product.route}>{product.label}</a>
               </li>
             ))}
           </AccordionContent>
         </AccordionItem>
       ))}
-      {/* Render a link to the personalize page with a 'new' badge */}
-      <Link
+      {/* Render a a to the personalize page with a 'new' badge */}
+      <a
         href='/personalizar'
         className='flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline relative'
       >
@@ -70,7 +61,7 @@ const SideNavbarFilter = () => {
         <span className='absolute top-2 -right-1 text-xs text-white bg-gray-900 rounded-full px-1.5 py-0.5 pointer-events-none'>
           nuevo
         </span>
-      </Link>
+      </a>
     </Accordion>
   )
 }
