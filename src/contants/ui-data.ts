9ed63f1ba83fb61798891f96067032aedbd29503
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-02 10:36:30 +0200

    feat: add comunidadesAutonomas constant for Spanish regions

---
+++ src/contants/ui-data.ts
@@ -0,0 +1,19 @@
+export const comunidadesAutonomas = [
+  'Andalucía',
+  'Aragón',
+  'Asturias',
+  'Baleares',
+  'Canarias',
+  'Cantabria',
+  'Castilla-La Mancha',
+  'Castilla y León',
+  'Cataluña',
+  'Comunidad Valenciana',
+  'Extremadura',
+  'Galicia',
+  'La Rioja',
+  'Madrid',
+  'Murcia',
+  'Navarra',
+  'País Vasco'
+]
