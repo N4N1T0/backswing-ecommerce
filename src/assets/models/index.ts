0efb0be640a5215b296c86ddeeb1808a2dc6791e
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-03 23:34:32 +0200

    refactor(models): uncomment and export model image imports

--- src/assets/models/index.ts
+++ src/assets/models/index.ts
@@ -1,6 +1,6 @@
-// import ModelA from './camisetas-mujer-model-a.webp'
-// import ModelC from './camisetas-mujer-model-c.webp'
-// import ModelD from './camisetas-mujer-model-d.webp'
-// import ModelE from './camisetas-mujer-model-e.webp'
+import ModelA from './camisetas-mujer-model-a.webp'
+import ModelC from './camisetas-mujer-model-c.webp'
+import ModelD from './camisetas-mujer-model-d.webp'
+import ModelE from './camisetas-mujer-model-e.webp'
 
-// export { ModelA, ModelC, ModelD, ModelE }
+export { ModelA, ModelC, ModelD, ModelE }
