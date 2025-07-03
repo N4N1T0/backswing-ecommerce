1d38d7f7a8ca0e083faedc62f3f6f8e1ceb38bda
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-03 17:23:02 +0200

    refactor(models): comment out unused model imports and exports
    The model image imports and exports are no longer being used in the application, so they have been commented out for potential future use rather than being deleted completely.

--- src/assets/models/index.ts
+++ src/assets/models/index.ts
@@ -1,6 +1,6 @@
-import ModelA from './camisetas-mujer-model-a.webp'
-import ModelE from './camisetas-mujer-model-e.webp'
-import ModelD from './camisetas-mujer-model-d.webp'
-import ModelC from './camisetas-mujer-model-c.webp'
+// import ModelA from './camisetas-mujer-model-a.webp'
+// import ModelC from './camisetas-mujer-model-c.webp'
+// import ModelD from './camisetas-mujer-model-d.webp'
+// import ModelE from './camisetas-mujer-model-e.webp'
 
-export { ModelA, ModelE, ModelD, ModelC }
+// export { ModelA, ModelC, ModelD, ModelE }
