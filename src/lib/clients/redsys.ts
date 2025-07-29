0b344534cb5e880f3e517e35e51b5fb3b420a2d0
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-07-29 11:24:11 +0200

    refactor(clients): simplify redsys client configuration by removing production urls
    The production environment is not currently being used, so we can simplify the configuration by removing the PRODUCTION_URLS option and always using SANDBOX_URLS.

--- src/lib/clients/redsys.ts
+++ src/lib/clients/redsys.ts
@@ -1,6 +1,6 @@
-import { createRedsysAPI, PRODUCTION_URLS, SANDBOX_URLS } from 'redsys-easy'
+import { createRedsysAPI, SANDBOX_URLS } from 'redsys-easy'
 
 export const { createRedirectForm, processRestNotification } = createRedsysAPI({
   secretKey: process.env.REDSYS_SECRET_KEY!,
-  urls: process.env.NODE_ENV === 'production' ? PRODUCTION_URLS : SANDBOX_URLS
+  urls: SANDBOX_URLS
 })
