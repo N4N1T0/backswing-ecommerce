a6dea4f3a45139f32dad11a2564f1c3a0ba9f1ca
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-04 18:31:59 +0200

    feat(commerce): add shipping configuration to commerce section
    Add new shipping configuration option under commerce section with Truck icon. Also standardize icon sizing by replacing 'h-4 w-4' with 'size-4' across all icons.

--- src/sanity/structure.tsx
+++ src/sanity/structure.tsx
@@ -16,6 +16,7 @@ import {
   Sparkle,
   StickyNote,
   Tag,
+  Truck,
   User,
   UserPen,
   UserRoundPen
@@ -28,72 +29,77 @@ export const structure: StructureResolver = (S) =>
     .items([
       S.listItem()
         .title('Contenido')
-        .icon(() => <BookOpen className='h-4 w-4' />)
+        .icon(() => <BookOpen className='size-4' />)
         .child(
           S.list()
             .title('Contenido del Sitio')
             .items([
               S.documentTypeListItem('legalPages')
                 .title('Paginas Legales')
-                .icon(() => <BookOpenText className='h-4 w-4' />),
+                .icon(() => <BookOpenText className='size-4' />),
               S.divider(),
               S.documentTypeListItem('post')
                 .title('Entradas')
-                .icon(() => <StickyNote className='h-4 w-4' />),
+                .icon(() => <StickyNote className='size-4' />),
               S.documentTypeListItem('author')
                 .title('Autores')
-                .icon(() => <UserRoundPen className='h-4 w-4' />),
+                .icon(() => <UserRoundPen className='size-4' />),
               S.documentTypeListItem('tag')
                 .title('Etiquetas')
-                .icon(() => <Tag className='h-4 w-4' />),
+                .icon(() => <Tag className='size-4' />),
               S.documentTypeListItem('subscriberNewsletter')
                 .title('Suscriptores')
-                .icon(() => <UserPen className='h-4 w-4' />)
+                .icon(() => <UserPen className='size-4' />)
             ])
         ),
       S.listItem()
         .title('Comercio')
-        .icon(() => <BadgeEuro className='h-4 w-4' />)
+        .icon(() => <BadgeEuro className='size-4' />)
         .child(
           S.list()
             .title('Productos & Ventas')
             .items([
               S.documentTypeListItem('product')
                 .title('Productos')
-                .icon(() => <ShoppingBasket className='h-4 w-4' />),
+                .icon(() => <ShoppingBasket className='size-4' />),
               S.documentTypeListItem('productCategory')
                 .title('Categorías')
-                .icon(() => <Filter className='h-4 w-4' />),
+                .icon(() => <Filter className='size-4' />),
               S.documentTypeListItem('coupon')
                 .title('Cupones')
-                .icon(() => <BadgeEuro className='h-4 w-4' />),
+                .icon(() => <BadgeEuro className='size-4' />),
               S.divider(),
               S.documentTypeListItem('costumer')
                 .title('Clientes')
-                .icon(() => <User className='h-4 w-4' />),
+                .icon(() => <User className='size-4' />),
               S.documentTypeListItem('order')
                 .title('Ordenes de Compra')
-                .icon(() => <CircleDollarSign className='h-4 w-4' />),
+                .icon(() => <CircleDollarSign className='size-4' />),
               S.documentTypeListItem('abandonedCart')
                 .title('Carritos de Compra')
-                .icon(() => <BaggageClaim className='h-4 w-4' />),
+                .icon(() => <BaggageClaim className='size-4' />),
               S.documentTypeListItem('wishlistType')
                 .title('Listas de Deseos')
-                .icon(() => <Heart className='h-4 w-4' />),
+                .icon(() => <Heart className='size-4' />),
               S.documentTypeListItem('noStockNotifyMe')
                 .title('No Stock Notificarme')
-                .icon(() => <Bell className='h-4 w-4' />),
+                .icon(() => <Bell className='size-4' />),
               S.divider(),
               S.documentTypeListItem('productDesigns')
                 .title('Diseño de Productos')
-                .icon(() => <Paintbrush className='h-4 w-4' />),
+                .icon(() => <Paintbrush className='size-4' />),
               S.documentTypeListItem('formats')
                 .title('Formatos de Diseños')
-                .icon(() => <BarChart className='h-4 w-4' />)
+                .icon(() => <BarChart className='size-4' />),
+              S.divider(),
+              S.listItem()
+                .title('Configuración de Envío')
+                .icon(() => <Truck className='size-4' />)
+                .child(S.document().schemaType('shipping'))
             ])
         ),
       S.listItem()
         .title('Funciones')
-        .icon(() => <Sparkle className='h-4 w-4' />)
+        .icon(() => <Sparkle className='size-4' />)
         .child(S.documentTypeList('featureFlags').title('Funciones'))
     ])
