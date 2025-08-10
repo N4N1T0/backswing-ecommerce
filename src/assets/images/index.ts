43bf416b483dfbc195621592e453e5a49e6d49f7
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-08-10 18:35:44 +0200

    feat(assets): add new images and reorganize exports
    - Add new image exports in assets/images/index.ts
    - Reorganize and sort exports alphabetically in personalization/index.ts

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+import KidImage from './Adobe Express - file (1).webp'
+import WomanImage from './Adobe Express - file (2).webp'
+import ManImage from './Adobe Express - file (3).webp'
+
+export { KidImage, ManImage, WomanImage }
--- src/assets/personalization/index.ts
+++ src/assets/personalization/index.ts
@@ -1,9 +1,16 @@
 import CamisetaMujer from './camiseta-mujer.webp'
-import CamisetaHombre from './camisetas-hombre.webp'
 import CamisetaNino from './camiseta-niño.webp'
-import SudaderasUnisex from './sudaderas-unisex.webp'
+import CamisetaHombre from './camisetas-hombre.webp'
 import SudaderasNino from './sudaderas-nino.webp'
+import SudaderasUnisex from './sudaderas-unisex.webp'
+
+// DESIGNS
 import FirstDay from './DISEÑO1.png'
+import Evolution from './DISEÑO10.png'
+import Extreme from './DISEÑO11.png'
+import FashionGirl from './DISEÑO12.png'
+import HardMatch from './DISEÑO13.png'
+import Family from './DISEÑO14.png'
 import LovePadel from './DISEÑO2.png'
 import StrongAndFurius from './DISEÑO3.png'
 import Mirones from './DISEÑO4.png'
@@ -12,30 +19,25 @@ import NoAge from './DISEÑO6.png'
 import Timming from './DISEÑO7.png'
 import JuegasConmigo from './DISEÑO8.png'
 import Locomotive from './DISEÑO9.png'
-import Evolution from './DISEÑO10.png'
-import Extreme from './DISEÑO11.png'
-import FashionGirl from './DISEÑO12.png'
-import HardMatch from './DISEÑO13.png'
-import Family from './DISEÑO14.png'
 
 export {
-	CamisetaMujer,
-	CamisetaHombre,
-	CamisetaNino,
-	SudaderasUnisex,
-	SudaderasNino,
-	FirstDay,
-	LovePadel,
-	StrongAndFurius,
-	Mirones,
-	Teenagers,
-	NoAge,
-	Timming,
-	JuegasConmigo,
-	Locomotive,
-	Evolution,
-	Extreme,
-	FashionGirl,
-	HardMatch,
-	Family,
+  CamisetaHombre,
+  CamisetaMujer,
+  CamisetaNino,
+  Evolution,
+  Extreme,
+  Family,
+  FashionGirl,
+  FirstDay,
+  HardMatch,
+  JuegasConmigo,
+  Locomotive,
+  LovePadel,
+  Mirones,
+  NoAge,
+  StrongAndFurius,
+  SudaderasNino,
+  SudaderasUnisex,
+  Teenagers,
+  Timming
 }
